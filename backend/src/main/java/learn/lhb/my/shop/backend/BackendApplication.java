5d440a6113dcbca0c8631e2570ea03e4ad5ac18e
lhb <782272323@qq.com>
2020-03-11 01:02:26 +0800

    建立了项目的基本骨架（后台和前端）

---
+++ backend/src/main/java/learn/lhb/my/shop/backend/BackendApplication.java
@@ -0,0 +1,16 @@
+package learn.lhb.my.shop.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 梁鸿斌
+ * @date 2020/3/11.
+ * @time 00:23
+ */
+@SpringBootApplication
+public class BackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
---
+++ backend/src/main/java/learn/lhb/my/shop/backend/service/test.java
@@ -0,0 +1,9 @@
+package learn.lhb.my.shop.backend.service;
+
+/**
+ * @author 梁鸿斌
+ * @date 2020/3/11.
+ * @time 00:31
+ */
+public class test {
+}
