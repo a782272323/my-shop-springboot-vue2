f00f80a2a952628539ce1dfc805913818706669e
lhb <782272323@qq.com>
2020-03-13 08:30:45 +0800

    前端框架换成了vue-element-admin,并简化了

--- vue-element-admin/src/router/index.js
+++ vue-element-admin/src/router/index.js
@@ -9,7 +9,6 @@ import Layout from '@/layout'
 /* Router Modules */
 import componentsRouter from './modules/components'
 import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
 import nestedRouter from './modules/nested'
 
 /**
@@ -188,7 +187,6 @@ export const asyncRoutes = [
   componentsRouter,
   chartsRouter,
   nestedRouter,
-  tableRouter,
 
   {
     path: '/example',
